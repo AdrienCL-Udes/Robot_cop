ef01eaa11a61b5981361170c7d3c21914fcb3624
Iliass Bourabaa <iliass.bourabaa@usherbrooke.ca>
2019-09-26 13:00:14 -0400

    Fonction tourner (essai)
    J'ai fait la fonction tourner pour des valeurs prédéfinis.  Il faudra la modifier car ce ne sont pas les bonnes valeurs

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,42 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+//Angle entre -90 et 90
+void tourner(int angle) {
+  //Robot tourne de 90 degrés vers la gauche
+  if(angle == -90){
+    MOTOR_SetSpeed(1, 0.6);
+
+    delay(300);
+
+    MOTOR_SetSpeed(1,0);
+
+  //Robot tourne de 45 degrés vers la gauche
+  }else if (angle == -45) {
+    MOTOR_SetSpeed(1, 0.6);
+
+    delay(150);
+
+    MOTOR_SetSpeed(1,0);
+
+  //Robot tourne de 45 degrés vers la droite
+  } else if(angle == 45) {
+    MOTOR_SetSpeed(0, 0.6);
+
+    delay(150);
+
+    MOTOR_SetSpeed(0,0);
+
+  //Robot tourne de 90 degrés vers la droite
+  } else if(angle == 90) {
+    MOTOR_SetSpeed(0, 0.6);
+
+    delay(300);
+
+    MOTOR_SetSpeed(0,0);
+  } else {
+    Serial.println("L'angle choisi ne fait pas partie des choix possibles");
+  }
+  
+}

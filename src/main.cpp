e04a3999a6e68a1f8c1deb4c607ebb894bd54343
Iliass Bourabaa <iliass.bourabaa@usherbrooke.ca>
2019-09-26 13:35:30 -0400

    Fonction calculerNbTour
    J'ai créé la fonction calculer le nombre de tour

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,22 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
+//Calcule le nombre de tour que de doit faire la roue selon les paramètres reçu par la fonction
+int calculerNbTour(int angle, int rayonRoue, int rayonArc) {
+  int nbTour;
+
+  nbTour = rayonArc * angle / (rayonRoue* 360);
+
+  return nbTour;
+}
+
 //Angle entre -90 et 90
 void tourner(int angle) {
+  
+  calculerNbTour(angle, 1.5, 8);
+
+  
+
   //Robot tourne de 90 degrés vers la gauche
   if(angle == -90){
     MOTOR_SetSpeed(1, 0.6);

9f6c49ba46eddd9cf868ca7be1f5f2322666377a
willbrq75 <55769064+willbrq75@users.noreply.github.com>
2019-09-26 10:48:12 -0400

    PID(Test 1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,58 @@
 #include <Arduino.h>
 #include <LibRobus.h>
+#define MASTERSPEED 0.5
 
-//EQUIPE 1 ICI
+float calc(int master, int slave)
+{
+  float diff = (master - slave)/float(master);
+  int percent;
+  if(diff>=0)
+  {
+    percent = 1 + diff;
+  }
+  else
+  {
+    percent = 1-diff;
+  }
+  float correction = MASTERSPEED*percent;
+  return correction;
 
-void setup() {
-  // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void PID()
+{
+  int wheelMaster = ENCODER_Read(1);
+  int wheelSlave = ENCODER_Read(0);
+  float correct = calc(wheelMaster,wheelSlave);
+  MOTOR_SetSpeed(1,MASTERSPEED);
+  MOTOR_SetSpeed(0,correct);
+}
+
+void reset()
+{
+  ENCODER_Reset(0);
+  ENCODER_Reset(1);
+  MOTOR_SetSpeed(1,MASTERSPEED);
+  MOTOR_SetSpeed(0,MASTERSPEED);
+}
+
+void WriteEncodeur()
+{
+  Serial.print("left wheel: ");
+  Serial.print(ENCODER_Read(0)+"\n");
+  Serial.print("Right wheel: ");
+  Serial.print(ENCODER_Read(1)+"\n");
+}
+
+void setup() 
+{
+  reset();
+  BoardInit();
+}
+
+void loop()
+{
+  PID();
+  WriteEncodeur();  
+  
 }

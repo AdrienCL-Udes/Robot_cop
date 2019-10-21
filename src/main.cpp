64251557d49ff000955776b63123ee9e07955336
Jean-Philippe Boucher <jeepsboucher@gmail.com>
2019-10-21 16:53:32 -0400

    Ajout de la librairie pour le capteur de couleur et test

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 #include <Arduino.h>
 #include <LibRobus.h>
+#include <Adafruit_TCS34725.h>
+
+#define anode false
+
+Adafruit_TCS34725 colorSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS,TCS34725_GAIN_4X);
+byte gammatable[256];
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  if(colorSensor.begin()){
+    Serial.println("Color sensor found");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if(ROBUS_IsBumper(REAR))
+  {
+    uint16_t red,green,blue, clear = 0;
+
+    colorSensor.enable();
+    colorSensor.getRawData(&red,&green,&blue,&clear);
+    float average = (red+green+blue)/3;
+    float r,g,b = 0;
+
+    r = red/average;
+    g = green/average;
+    b = blue/average;
+    Serial.print("The RGB value are: RGB( ");
+    Serial.print(r);
+    Serial.print(", ");
+    Serial.print(g);
+    Serial.print(", ");
+    Serial.print(b);
+    Serial.println(" )");
+  } 
 }

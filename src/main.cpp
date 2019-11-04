3ff38073fe6dfb359c132fac44860e9e2ee52378
marquis <marquis84@hotmail.ca>
2019-11-04 15:53:22 -0500

    capteur distance

--- src/main.cpp
+++ src/main.cpp
@@ -3,100 +3,41 @@
 
 //programmation capteur de proximité marquis
 #define capteur0 A0
-#define capteur1 A1
-void setup()
+// #define capteur1 A1
+void capteurDistance(int distancemin)
 {
-Serial.begin(9600);
-}
-void loop()
-{
-  int sensorPins[]= {capteur0, capteur1}; //array contenant les pins pour chq capteur IR
-  int distanceCapteurs[2]; //initialisation d'un array pour storer les valeurs de chq capteur
-  int id=0; //initialisation de la variable id
   int roue, roue2;
-
-  for (int i = 0; i < 2; i++) //loop de 0 a 1 pour passer a travers chq capteur IR
+  int resultatCm, mesure;
+  mesure = analogRead(capteur0);
+  resultatCm=(6787.0/(mesure-3.0))-4.0;
+  while(resultatCm >= distancemin)
   {
-    id = sensorPins[i]; //assigner le nom du pin a la variable id pour le capteur d'interet
-    distanceCapteurs[i] = ROBUS_ReadIR(id); //assigner la sortie (distance) de ROBUS_READIR au array de valeur mesurees
-  
-    if(distanceCapteurs[i]<10) //distance minimal mesuré par le capteur (a tester)
-    {
-      MOTOR_SetSpeed(roue, 0); //initialisation de la vitesse a 0
-      MOTOR_SetSpeed(roue2, 0); //initialisation de la vitesse a 0
-    }
+    Serial.print(resultatCm);
+     Serial.print("\n");
+      if(resultatCm <= distancemin)
+      {
+        MOTOR_SetSpeed(roue,0);
+        MOTOR_SetSpeed(roue2,0);
+      }
+      delay(1000);
+    mesure = analogRead(capteur0);
+    resultatCm=(6787.0/(mesure-3.0))-4.0;
   }
-}
-int ROBUS_ReadIR(uint8_t id)
 
+}
+//int ROBUS_ReadIR(uint8_t id)
 
 //EQUIPE 2 ICI
-// Constant use for tuning PID
+//Constant use for tuning PID
 const float Pfactor = 0.0002;
 const float Ifactor = 0.00001;
 
 // Init varaible for motor control
 int integral = 0;
-const float init_speed = 0.4;
+const float init_speed = 0.6;
 float current_speed = init_speed;
 const float Wheel_size_10 = 76.2;
 const int numberTickWheel = 32000;
-
-//Calcule le nombre de pulse que doit faire la roue selon les paramètres reçus par la fonction
-//rayonRoue et rayonArc sont en cm et angle est en degrès
-float calculerNbPulse(int angle, float rayonRoue, float rayonArc) {
-  float nbPulse;
-
-  nbPulse =  (rayonArc * angle* 3200)/(360*rayonRoue);
-  
-  return nbPulse;
-}
-
-//Cette fonction fait tourner le robot avec les deux moteurs avec le sens et l'angle entré par l'utilisateur
-//roue = 0, le robot tourne vers la droite
-//+roue = 1, le robot tourne vers la gauche
-//angle est en degré
-void tourner1Roue(unsigned int angle, int roue) {
-  int pulseEncodeur = 0, pulse;
-  ENCODER_Reset(roue);
-  
-  pulse = calculerNbPulse(angle, 7.62/2 , 19.3);
-
-  MOTOR_SetSpeed(roue, 0.3);
-
-  while (pulseEncodeur < pulse) {
-    pulseEncodeur = ENCODER_Read(roue);
-  }
-
-  MOTOR_SetSpeed(roue, 0);
-}
-
-//Cette fonction fait tourner le robot avec les deux moteurs avec le sens et l'angle entré par l'utilisateur
-//roue = 0, le robot tourne vers la droite
-//+roue = 1, le robot tourne vers la gauche
-//angle est en degré
-void tourner2Roue(unsigned int angle, int roue) {
-  int pulseEncodeur = 0, pulse, roue2;
-  ENCODER_Reset(roue);
-  
-  pulse = calculerNbPulse(angle/2, 7.62/2 , 19.3);
-
-  if(roue == 0){
-    roue2 = 1;
-  }else {
-    roue2 = 0;
-  }
-  
-  MOTOR_SetSpeed(roue, 0.2);
-  MOTOR_SetSpeed(roue2, -0.2);
-
-  while (pulseEncodeur < pulse-50) {
-    pulseEncodeur = ENCODER_Read(roue);
-  }
-
-  MOTOR_SetSpeed(roue, 0);
-  MOTOR_SetSpeed(roue2, 0);
-}
 
 // This fonction will adjuste the speed of motor 0
 // to match motor 1
@@ -122,46 +63,21 @@ void PID()
   delay(100);
 }
 
-int CALCUL_nbCompleteWheelRotation_10(float cm_distance)
+void tourner(int angle, int roue1, int roue2)
 {
-  return cm_distance / Wheel_size_10;
-}
-
-int CALCUL_nbPartialWheelRotation(float cm_distance, int nbCompleteRotation)
-{
-  float leftover = cm_distance - nbCompleteRotation * Wheel_size_10;
-  return leftover * numberTickWheel / Wheel_size_10;
-}
-
-void MOVE_forward(int distance)
-{
-  int nbTour_10 = CALCUL_nbCompleteWheelRotation_10(distance);
-  int nbTick = CALCUL_nbPartialWheelRotation(distance, nbTour_10);
-
-  Serial.print("Nb Tour: ");
-  Serial.println(nbTour_10);
-  Serial.print("Nb Tick: ");
-  Serial.println(nbTick);
-  
-  int tmpTour = 0;
   ENCODER_Reset(0);
   ENCODER_Reset(1);
-  MOTOR_SetSpeed(0, init_speed);
-  MOTOR_SetSpeed(1, init_speed);
-  while (nbTour_10 > tmpTour || ENCODER_Read(0) <= nbTick)
-  {
-    PID();
-    if (ENCODER_Read(0) > 32000)
-    {
-      tmpTour = tmpTour + 1;
-      ENCODER_Reset(0);
-      ENCODER_Reset(1);
-    }
-  }
-  MOTOR_SetSpeed(0, 0);
-  MOTOR_SetSpeed(1, 0);
-}
+  int nombreTick = 20 * angle * 3200 / (360 * 8) / 2;
 
+  MOTOR_SetSpeed(roue1, init_speed);
+  MOTOR_SetSpeed(roue2, init_speed * -1);
+  while (nombreTick <= ENCODER_Read(roue1))
+  {
+    //wait
+  }
+  MOTOR_SetSpeed(roue1, 0);
+  MOTOR_SetSpeed(roue2, 0);
+}
 //PUT THESE AT THE END
 void setup()
 {
@@ -173,60 +89,14 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-<<<<<<< HEAD
-  /// loll
-=======
   while (1)
   {
-    if (ROBUS_IsBumper(2))
+    capteurDistance(15);
+
+    if (ROBUS_IsBumper(3))
     {
-      MOVE_forward(30);
-      integral = 0;
-      current_speed = init_speed;
-    }
-    else if (ROBUS_IsBumper(3))
-    {
-      MOVE_forward(122);
-
-      tourner2Roue(90, 1);
-
-      MOVE_forward(90);
-      
-      tourner2Roue(90, 0);
-
-      MOVE_forward(70);
-
-      tourner2Roue(45, 0);
-
-      MOVE_forward(172);
-
-      tourner2Roue(90, 1);
-
-      MOVE_forward(45);
-
-      tourner2Roue(45,0);
-
-      MOVE_forward(110);
-    }
-    else if(ROBUS_IsBumper(0)) {
-      tourner1Roue(90, 0);
-      delay(1000);
-      tourner2Roue(90, 0);
-      delay(1000);
-      tourner1Roue(90, 0);
-      delay(1000);
-      tourner2Roue(90, 0);
-      delay(1000);
-      /*tourner(360, 0);
-      delay(1000);
-
-      tourner(45, 1);
-      delay(1000);
-
-      tourner(90, 1);
-      delay(1000);*/
+      tourner(90, 0, 1);
+      tourner(90, 1, 0);
     }
   }
->>>>>>> equipe_2
 }
-

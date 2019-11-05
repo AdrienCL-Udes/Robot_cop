1fe025e94bf7f15ac78a20baf302edb6a9ff194c
Jean-Philippe Boucher <jeepsboucher@gmail.com>
2019-11-04 20:29:06 -0500

    Capteur couleur fonctionnel

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,57 @@
 #include <Arduino.h>
 #include <LibRobus.h>
 #include <Adafruit_TCS34725.h>
-
-#define anode false
+// red == 0, green == 1, blue == 2, , yellow == 3
 
 Adafruit_TCS34725 colorSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS,TCS34725_GAIN_4X);
-byte gammatable[256];
+
+int getColorFromSensor()
+{
+  uint16_t red,green,blue, clear = 0;
+  int r = 0; int g = 1; int b = 2; int y = 3;
+
+  colorSensor.enable();
+  colorSensor.getRawData(&red,&green,&blue,&clear);
+
+  Serial.print("The RGB value are: TEST( ");
+  Serial.print(red);
+  Serial.print(", ");
+  Serial.print(green);
+  Serial.print(", ");
+  Serial.print(blue);
+  Serial.println(" )");
+
+  if(red > green && red > blue)
+  {
+    if(red > 125 && green < 100 && blue < 105)
+    {
+      return r;
+    }
+  }
+  if(green > red && green > blue)
+  {
+    if(green <= 125 && green >= 110 && blue > 100)
+    {
+      return g;
+    }
+  }
+  if(blue > red && blue > green)
+  {
+    if(blue <= 160 && blue >= 135)
+    {
+      return b;
+    }
+    
+  }
+  if(red > blue && green > blue)
+  {
+    if(red >= 250 && green > 240)
+    {
+      return y;
+    }
+  }
+  return 1000;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,24 +64,5 @@ void setup() {
 
 void loop()
 {
-  if(ROBUS_IsBumper(REAR))
-  {
-    uint16_t red,green,blue, clear = 0;
-
-    colorSensor.enable();
-    colorSensor.getRawData(&red,&green,&blue,&clear);
-    float average = (red+green+blue)/3;
-    float r,g,b = 0;
-
-    r = red/average;
-    g = green/average;
-    b = blue/average;
-    Serial.print("The RGB value are: RGB( ");
-    Serial.print(r);
-    Serial.print(", ");
-    Serial.print(g);
-    Serial.print(", ");
-    Serial.print(b);
-    Serial.println(" )");
-  } 
+  
 }

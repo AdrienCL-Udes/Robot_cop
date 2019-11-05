27c02d968087881db5ba2c4d055ee5fe72129e66
AdrienCL-Udes <cota3210@usherbrooke.ca>
2019-11-05 15:54:13 -0500

    half baked code

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,18 @@
 #include <Arduino.h>
 #include <LibRobus.h>
-#define MASTERSPEED 0.3
-#define ROTATION 3200.00
-#define TICK_CM 133.7233
-int lastMaster = 0;
 
-void acc()
-{
-  float speed = 0.0;
-  while (speed < MASTERSPEED)
-  {
-    MOTOR_SetSpeed(0, speed);
-    MOTOR_SetSpeed(1, speed);
-    speed += 0.05;
-    delay(50);
-  }
-}
-void dec()
-{
-  float speed = MASTERSPEED;
-  while (speed > 0)
-  {
-    MOTOR_SetSpeed(0, speed);
-    MOTOR_SetSpeed(1, speed);
-    speed -= 0.05;
-    delay(50);
-  }
-}
+#define PIN_RIGHT A0
+#define PIN_LEFT A1
+#define PIN_MIDDLE A2
+#define PIN_DISTANCE A3
 
-void PID();
+#define PIN_NB 3
+#define SPEED -0.2
 
-void forward(float cm)
+void move(float speed)
 {
-  bool end = false;
-  acc();
-  while (end == false)
-  {
-    PID();
-    if (ENCODER_Read(1) >= (TICK_CM * cm) - 200)
-    {
-      end = true;
-      dec();
-      ENCODER_Reset(0);
-      ENCODER_Reset(1);
-    }
-  }
+  MOTOR_SetSpeed(RIGHT, speed);
+  MOTOR_SetSpeed(LEFT, speed);
 }
 
 //Calcule le nombre de pulse que doit faire la roue selon les paramètres reçus par la fonction
@@ -57,28 +25,6 @@ float calculerNbPulse(int angle, float rayonRoue, float rayonArc)
 
   return nbPulse;
 }
-
-//Cette fonction fait tourner le robot avec les deux moteurs avec le sens et l'angle entré par l'utilisateur
-//roue = 0, le robot tourne vers la droite
-//+roue = 1, le robot tourne vers la gauche
-//angle est en degré
-void tourner1Roue(unsigned int angle, int roue)
-{
-  int pulseEncodeur = 0, pulse;
-
-  pulse = calculerNbPulse(angle, 7.62 / 2, 19.3);
-
-  MOTOR_SetSpeed(roue, 0.3);
-
-  while (pulseEncodeur < pulse)
-  {
-    pulseEncodeur = ENCODER_Read(roue);
-  }
-
-  MOTOR_SetSpeed(roue, 0);
-  ENCODER_Reset(roue);
-}
-
 //Cette fonction fait tourner le robot avec les deux moteurs avec le sens et l'angle entré par l'utilisateur
 //roue = 0, le robot tourne vers la droite
 //+roue = 1, le robot tourne vers la gauche
@@ -86,6 +32,7 @@ void tourner1Roue(unsigned int angle, int roue)
 void tourner2Roue(unsigned int angle, int roue)
 {
   int pulseEncodeur = 0, pulse, roue2;
+  ENCODER_Reset(roue);
 
   pulse = calculerNbPulse(angle / 2, 7.62 / 2, 19.3);
 
@@ -101,92 +48,255 @@ void tourner2Roue(unsigned int angle, int roue)
   MOTOR_SetSpeed(roue, 0.2);
   MOTOR_SetSpeed(roue2, -0.2);
 
-  while (pulseEncodeur < pulse - 100)
+  while (pulseEncodeur < pulse - 50)
   {
     pulseEncodeur = ENCODER_Read(roue);
   }
 
   MOTOR_SetSpeed(roue, 0);
   MOTOR_SetSpeed(roue2, 0);
-  ENCODER_Reset(roue);
-  ENCODER_Reset(roue2);
 }
 
-float calc(int master, int slave)
+bool getPinState(int8_t pin)
 {
-  float correction = MASTERSPEED;
-  float diff;
-  diff = (master - slave) / ROTATION;
-  Serial.println(diff);
+  return analogRead(pin) > 650;
+}
 
-  if (diff > 0)
+bool followLine()
+{
+  bool pinRight = getPinState(PIN_RIGHT);
+  bool pinLeft = getPinState(PIN_LEFT);
+  bool pinMiddle = getPinState(PIN_MIDDLE);
+
+  if (pinMiddle && !pinRight && !pinLeft)
   {
-    correction = correction * (1.00 + diff);
-  }
-  else if (diff < 0)
-  {
-    correction = correction * (1.00 + diff);
+    move(SPEED);
   }
 
-  return correction;
-}
-
-void PID()
-{
-
-  lastMaster = ENCODER_Read(1);
-  int wheelSlave = ENCODER_Read(0);
-  float correct = calc(lastMaster, wheelSlave);
-  Serial.print("slave speed: ");
-  Serial.println(correct);
-  MOTOR_SetSpeed(1, MASTERSPEED);
-  MOTOR_SetSpeed(0, correct);
-}
-
-void go(int *tabData, char *tabMove, int tabSize)
-{
-  for (int i = 0; i < tabSize; i++)
+  if (pinRight && !pinLeft)
   {
-    switch (tabMove[i])
-    {
-    case 'F':
-      forward(tabData[i]);
-      break;
+    MOTOR_SetSpeed(RIGHT, 0);
+    MOTOR_SetSpeed(LEFT, SPEED);
+  }
 
-    case 'R':
-      tourner2Roue(tabData[i], 0);
-      break;
+  if (pinLeft && !pinRight)
+  {
+    MOTOR_SetSpeed(LEFT, 0);
+    MOTOR_SetSpeed(RIGHT, SPEED);
+  }
 
-    case 'L':
-      tourner2Roue(tabData[i], 1);
-      break;
+  if (!pinMiddle && !pinRight && !pinLeft)
+  {
+    MOTOR_SetSpeed(LEFT, SPEED);
+    MOTOR_SetSpeed(RIGHT, SPEED);
+  }
 
-    default:
-      break;
-    }
+  if (pinMiddle && pinRight && pinLeft)
+  {
+    return false;
+  }
 
+  return true;
+}
+
+//COLOR
+//Vert = 0
+//Bleu = 1
+//Jaune = 2
+//Rouge = 3
+void goToColor(int color)
+{
+  switch (color)
+  {
+  case 0:
     delay(300);
+    move(-0.2);
+    delay(3000);
+    move(0.0);
+    delay(300);
+
+    tourner2Roue(45, LEFT);
+
+    delay(200);
+
+    move(SPEED);
+    do
+    {
+      delay(100);
+    } while (followLine());
+
+    move(0.0);
+
+    delay(200);
+
+    move(0.2);
+
+    delay(500);
+
+    move(0.0);
+
+    tourner2Roue(160, RIGHT);
+
+    delay(200);
+
+    move(0.2);
+
+    delay(1000);
+    break;
+
+  case 1:
+    delay(300);
+    move(-0.2);
+    delay(2000);
+    move(0.0);
+    delay(300);
+
+    tourner2Roue(135, LEFT);
+
+    delay(200);
+
+    move(SPEED);
+
+    delay(3000);
+
+    tourner2Roue(30, LEFT);
+
+    do
+    {
+      delay(100);
+    } while (followLine());
+
+    move(0.0);
+
+    delay(200);
+
+    move(0.2);
+
+    delay(500);
+
+    move(0.0);
+
+    tourner2Roue(160, RIGHT);
+
+    delay(200);
+
+    move(0.2);
+
+    delay(1000);
+    break;
+
+  case 2:
+    tourner2Roue(45, 1);
+    break;
+
+  case 3:
+    delay(300);
+    move(-0.2);
+    delay(3000);
+    move(0.0);
+    delay(300);
+
+    tourner2Roue(30, RIGHT);
+
+    delay(200);
+
+    move(SPEED);
+    do
+    {
+      delay(100);
+    } while (followLine());
+
+    move(0.0);
+
+    delay(200);
+
+    move(0.2);
+
+    delay(500);
+
+    move(0.0);
+
+    tourner2Roue(190, LEFT);
+
+    delay(200);
+
+    move(0.2);
+
+    delay(1000);
+    break;
+
+  default:
+    break;
   }
+
+  move(0.0);
+  delay(200);
+  SERVO_SetAngle(0, 80);
+}
+
+void goGrabBall()
+{
+  delay(200);
+  tourner2Roue(45, 0);
+  delay(200);
+
+  MOTOR_SetSpeed(RIGHT, 0.2);
+  MOTOR_SetSpeed(LEFT, -0.2);
+
+  Serial.println(analogRead(PIN_DISTANCE));
+  while (analogRead(PIN_DISTANCE) < 150)
+  {
+    delay(50);
+    //wait
+  }
+
+  delay(100);
+  move(0.0);
+
+  delay(200);
+
+  move(0.2);
+
+  while (analogRead(PIN_DISTANCE) < 300)
+  {
+    delay(50);
+    //wait
+  }
+
+  while (analogRead(PIN_DISTANCE) >= 200)
+  {
+    delay(50);
+    //wait
+  }
+
+  delay(100);
+  move(0.0);
+
+  SERVO_SetAngle(0, 125);
+  delay(500);
 }
 
 void setup()
 {
   BoardInit();
+  pinMode(PIN_RIGHT, INPUT_PULLUP);
+  pinMode(PIN_LEFT, INPUT_PULLUP);
+  pinMode(PIN_MIDDLE, INPUT_PULLUP);
+  pinMode(PIN_DISTANCE, INPUT);
+
+  SERVO_Enable(0);
+  SERVO_SetAngle(0, 80);
 }
 
 void loop()
 {
-  if (ROBUS_IsBumper(3))
+  delay(1000);
+  goGrabBall();
+
+  goToColor(1);
+
+  while (1)
   {
-    //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-    int tabData[] =  {223,  90, 92,  100, 50,  90,   50,  85,  95,  90,  42,  90, 120, 180, 248};
-    char tabMove[] = {'F', 'L', 'F', 'R', 'F', 'R', 'F', 'L', 'F', 'R', 'F', 'L', 'F', 'L', 'F'};
-
-    int tabSize = sizeof(tabData);
-
-    int *pointerTabData = tabData;
-    char *pointerTabMove = tabMove;
-
-    go(pointerTabData, pointerTabMove, tabSize);
+    delay(60000);
   }
 }
